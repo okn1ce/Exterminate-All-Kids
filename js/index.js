03a2944debd9ad73d840ec33b1cbad467b1bf77b
okn1ce <50502437+okn1ce@users.noreply.github.com>
2019-05-12 04:43:46 +0100

    Added "Terrorist" upgrade.
    Terrorist upgrade kills 5 kids/s.

--- js/index.js
+++ js/index.js
@@ -1,9 +1,20 @@
+//Variables
 var kids = 0;
 var soldier = 0;
 var soldierCost = 10;
+var SoldierKidsPerSecond = 0;
 var guns = 0;
 var gunCost = 130;
-var SoldierKidsPerSecond = 0;
+var terrorist = 0;
+var TerroristCost = 600;
+var TerroristsKidsPerSecond = 0;
+var bomb = 0;
+var bombCost = 900;
+
+
+
+
+
 
 function prettify(input){
     var output = Math.round(input * 1000000)/1000000;
@@ -15,6 +26,9 @@ function killKid() {
   document.getElementById("kidCount").innerHTML = prettify(kids);
 }
 
+
+
+//Soldier & Gun upgrades
 function buySoldier() {
   if (kids >= soldierCost) {
     soldier = soldier + 1;
@@ -24,13 +38,8 @@ function buySoldier() {
   document.getElementById("soldierCount").innerHTML = soldier;
   document.getElementById("kidCount").innerHTML = prettify(kids);
   document.getElementById("soldierCost").innerHTML = prettify(soldierCost);
-  }
-}
-
-function Tick() {
-  kids = kids + SoldierKidsPerSecond;
-  document.getElementById("kidCount").innerHTML = prettify(kids);
   document.getElementById("soldierSpeed").innerHTML = SoldierKidsPerSecond;
+  }
 }
 
 function buyGun() {
@@ -40,9 +49,33 @@ function buyGun() {
     SoldierKidsPerSecond = SoldierKidsPerSecond * 2;
     gunCost = ((gunCost + 1) * 2);
     document.getElementById("gunCost").innerHTML = gunCost;
-    
-    
-    
+    document.getElementById("soldierSpeed").innerHTML = SoldierKidsPerSecond;  
+  }
+}
+
+
+//Terrorist & Bomb upgrades
+function buyTerrorist() {
+  if (kids >= TerroristCost) {
+    terrorist = terrorist + 1;
+    TerroristsKidsPerSecond = TerroristsKidsPerSecond + 5;
+    kids = kids - TerroristCost;
+    TerroristCost = TerroristCost + ((terrorist + 1) * 150)
+  document.getElementById("terroristCount").innerHTML = terrorist;
+  document.getElementById("kidCount").innerHTML = prettify(kids);
+  document.getElementById("terroristCost").innerHTML = prettify(TerroristCost);
+  document.getElementById("terroristSpeed").innerHTML = (TerroristsKidsPerSecond);
+  }
+}
+
+function buyBomb() {
+  if (kids >= bombCost) {
+    bomb = bomb + 1;
+    kids = kids - bombCost;
+    TerroristsKidsPerSecond = TerroristsKidsPerSecond * 2;
+    bombCost = ((bombCost + 1) * 2);
+    document.getElementById("bombCost").innerHTML = bombCost; 
+    document.getElementById("terroristSpeed").innerHTML = TerroristsKidsPerSecond;   
   }
 }
 
@@ -50,6 +83,18 @@ function buyGun() {
 
 
 
+
+
+
+
+
+//Ticks & kids per second
+
+function Tick() {
+  kids = kids + SoldierKidsPerSecond + TerroristsKidsPerSecond;
+  document.getElementById("kidCount").innerHTML = prettify(kids);
+  ;
+}
 
 window.setInterval(function(){
 	
